bd86e2fa322c06c61caac8ca9888f1fa810bc602
Shivam Gupta_ <shivamgupta34100@gmail.com>
2024-10-05 22:25:07 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb+srv://shivamgupta34100:JzDhwCaTUN05AU8t@cluster0.vrmyc.mongodb.net/")
+mongoose.connect("")
 const userSchema= mongoose.Schema({
       title : String,
       description : String,
